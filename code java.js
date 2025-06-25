af132e1ec01de9c66d358bf8761b081d6563e297
shehabalgbri <153760149+shehabalgbri@users.noreply.github.com>
2025-06-25 17:34:55 +0300

    Add files via upload

---
+++ code java.js
@@ -0,0 +1,6 @@
+friend=new Array;
+friend[0].F_Name="shehab";
+friend[0].L_Name="algbri";
+friend[0].Age=25;
+friend[0].jop="IT";
+alert("The Name :"+friend[0].F_Name +" "+ friend[0].L_Name+"\n" +"The Age :"+friend[0].Age);
